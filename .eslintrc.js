bc67f79c10176e8c384abec845c9b1b18b7ae712
Chin Ying <scy.6char@gmail.com>
2024-02-06 00:49:53 +0800

    chore: add sort imports package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: false,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   extends: [
     'eslint:recommended', // Recommended ESLint rules
     'plugin:@typescript-eslint/eslint-recommended', // Disables rules from `eslint:recommended` that are already covered by the TypeScript typechecker
@@ -62,5 +62,8 @@ module.exports = {
         allow: ['warn', 'error'],
       },
     ],
+
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
 }
