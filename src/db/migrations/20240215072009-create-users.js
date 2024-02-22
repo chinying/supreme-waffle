f7e0562fe527886999c86b93474a37f263525a8f
Chin Ying <scy.6char@gmail.com>
2024-02-22 19:50:03 +0800

    feat: add migration file for users

---
+++ src/db/migrations/20240215072009-create-users.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users')
+  }
+};
