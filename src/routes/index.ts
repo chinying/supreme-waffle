084c3da3b53991a4d14801cf8fa00400ce9f8295
Chin Ying <scy.6char@gmail.com>
2024-10-29 10:25:36 +0800

    feat: add dummy endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,11 @@ import { Request, Response, Router } from 'express'
 
 const router = Router()
 
+router.post('/info', (req: Request, res: Response) => {
+  console.log({ timestamp: new Date(), message: JSON.stringify(req.body) })
+  res.sendStatus(200)
+})
+
 router.get('/', (_req: Request, res: Response) => {
   res.status(200).json({ message: 'ok' })
 })
